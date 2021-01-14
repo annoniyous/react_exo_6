c0bcc5b93db64d03e9e8a9681b57d28990154eb1
annoniyous <bouysantiman.y@outlook.com>
2021-01-14 23:34:46 +0100

    ADD_E_L fini

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,11 +3,20 @@ import React, { Component } from 'react'
 
 export class Navigation extends Component {
     render() {
+        let link1 = () =>{
+            console.log('Acceuil')
+        };
+        let link2 = () =>{
+            console.log('Galerie')
+        }
+        let link3 = () =>{
+            console.log('Contact')
+        }
         return (
             <div id="navList">
-                <button>Link 1</button>
-                <button>Link 2</button>
-                <button>Link 3</button>
+                <button onClick={link1} id="link1">Link 1</button>
+                <button onMouseOver={link2} id="link2">Link 2</button>
+                <button onDoubleClick={link3} id="link3">Link 3</button>
             </div>
         )
     }
